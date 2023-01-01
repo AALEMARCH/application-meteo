3931ec39029edd6c103b0ca207bc54700e19f69f
Antoine Lemarchand <lemarchand.antoine00@gmail.com>
2023-01-01 19:17:34 +0100

    appel api meteo actuelle et meteo sur cinq jours / mise en place de certaines valeurs dans le html

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ btn[0].addEventListener("click", (e) => {
 
   let geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&appid=${APIKEY}`;
 
+  // geocodage du nom de la ville en latitude et longitude
   fetch(geoUrl)
     .then((res) => res.json())
     .then((data) => {
@@ -22,7 +23,7 @@ btn[0].addEventListener("click", (e) => {
       htmlValue.textContent = data[0].name;
 
       // Creer une page au click sur IQA avec tous les indices de pollution detailler
-
+      // appel api des donnée de polution
       fetch(
         `http://api.openweathermap.org/data/2.5/air_pollution?lat=${data[0].lat}&lon=${data[0].lon}&appid=${APIKEY}`
       )
@@ -38,16 +39,76 @@ btn[0].addEventListener("click", (e) => {
           console.log(IQ.list[0].components.pm10); // matières particulaires grossières µg/m³
           console.log(IQ.list[0].components.nh3); // Ammoniac µg/m³
 
-          const iqaValue = document.querySelector(".hautDePage__indice");
-          iqaValue.textContent = `IQA ${IQ.list[0].main.aqi}`;
+          const iqaValueS = document.querySelector(".hautDePage__indice");
+
+          // Switch Case pour la selection des valeur d'indice de qualité de l'air
+          let valeur = IQ.list[0].main.aqi;
+          if (valeur != "") {
+            switch (valeur) {
+              case 1:
+                iqaValueS.textContent = `IQA 1 - bon`;
+                break;
+              case 2:
+                iqaValueS.textContent = `IQA 2 - passable`;
+                break;
+              case 3:
+                iqaValueS.textContent = `IQA 3 - modéré`;
+                break;
+              case 4:
+                iqaValueS.textContent = `IQA 4 - mauvais`;
+                break;
+              case 5:
+                iqaValueS.textContent = `IQA 5 - très mauvais`;
+                break;
+              default:
+                iqaValueS.textContent = `IQA - indisponible`;
+            }
+          } else {
+            iqaValueS.textContent = `IQA - indisponible`;
+          }
         });
 
+      // appel api de la météo actuelle et conversion en degree celcius
       fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&appid=${APIKEY}&units=metric`
+        `https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&appid=${APIKEY}&units=metric&lang=fr`
       )
         .then((res) => res.json())
         .then((meteo) => {
           console.log(meteo);
+
+          // ont défini les selecteur avec un S comme Selecteur
+          const meteoS = document.querySelector(".hautDePage__temps");
+          const temperatureS = document.querySelector(
+            ".hautDePage__degre--style"
+          );
+          const ressentieS = document.getElementById("ressentie");
+          const humiditeS = document.getElementById("humidite");
+          const pressionS = document.getElementById("pression");
+          const vitesseVentS = document.getElementById("vent");
+
+          // ont crée des variables pour les valeurs numériques afin d'arrondir le resultat à l'entier le plus proche
+          let degValue = meteo.main.temp;
+          let ressentieValue = meteo.main.feels_like;
+          let humiditeValue = meteo.main.humidity;
+          let pressionValue = meteo.main.pressure;
+          let vitesseVentValue = meteo.wind.speed;
+
+          // ont écrit le résultat dans le html
+          meteoS.textContent = meteo.weather[0].description;
+          temperatureS.textContent = Math.round(degValue);
+          ressentieS.textContent = Math.round(ressentieValue);
+          humiditeS.textContent = Math.round(humiditeValue);
+          pressionS.textContent = Math.round(pressionValue);
+          vitesseVentS.textContent = Math.round(vitesseVentValue * 100) / 100;
+        });
+
+      // Appel API sur 5 jours avec une latence de 3 heures
+      fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=${APIKEY}`
+      )
+        .then((res) => res.json())
+        .then((prevision) => {
+          console.log(prevision);
         });
     });
 });
